0d82d666077507314a742fd75abfde227f2c0918
Oksana <5oks5@mail.com>
2022-08-03 18:38:36 +0400

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = ['(', '{', '[' , '|', '1', '3', '5', '7', '8'];
+  const bracketsPair = {
+    [')'] : '(',
+    ['}'] : '{',
+    [']'] : '[',
+    ['|'] : '|',
+    ['2'] : '1',
+    ['4'] : '3',
+    ['6'] : '5',
+    ['7'] : '7',
+    ['8'] : '8',
+  };
+    let stack = [];
+    for(let i = 0; i < str.length; i++) {
+    let activateSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+      if(stack.length !== 0 && bracketsPair[activateSymbol] === topElement) {
+        stack.pop();
+      } else {
+        stack.push(activateSymbol)
+        }
+
+        
+
 }
+  return stack.length === 0;
+  
+}
+
